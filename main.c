4d7c5a22cb7cec113ca95019c2692aa355db6e7b
Gerard Rozsavolgyi <roza@univ-orleans.fr>
2023-10-17 12:42:46 +0200

    ajout main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include"listepers.h"
+
+int main()
+{
+    struct Personne pers;
+    Listep lp;
+    cree_liste(&lp);
+    printf("\n Saisie 1ere personne :");
+    saisir_personne(&pers);
+    inserer_ordre(pers, &lp);
+    printf("\n Saisie 2eme personne :");
+    saisir_personne(&pers);
+    inserer_ordre(pers, &lp);
+    printf("\n");
+    affiche_liste(lp);
+    printf("\n Saisie 3eme personne :");
+    saisir_personne(&pers);
+    inserer_ordre(pers, &lp);
+    affiche_liste(lp);
+    sauvegarde_liste(lp, "liste.pers");
+    liberer_liste(lp);
+    Listep lp2;
+    restaure_liste(&lp2, "liste.pers");
+    affiche_liste(lp2);
+    liberer_liste(lp2);
+    getchar(); // pour  consommer le '\n' restant
+    return 0;
+}
